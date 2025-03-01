bf61751dcae58eaf387055224bce4257fa83b26f
jose_belchi_ <josebelchihernandez@gmail.com>
2025-03-01 12:37:38 +0100

    Skeleton and retrieval functions done

--- src/main/java/com/PhonexConvert/Main.java
+++ src/main/java/com/PhonexConvert/Main.java
@@ -3,22 +3,31 @@ package main.java.com.PhonexConvert;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args){
-
-    }
-
-    public static void Phonex(){
         Scanner input = new Scanner(System.in);
         String phrase = "";
 
-        do{
+        do {
             if (phrase.isEmpty()){
                 System.out.println("-=- Phonetic Alphabet Convert -=-");
             }
 
-            System.out.println("Quiere pasar de texto natural a Alfabeto fonético (1) o al revés (2)?");
+            System.out.println("Quiere pasar de texto natural a Alfabeto fonético (\"1\") o al revés (\"2\")? (\"0\" para salir)");
             phrase = input.nextLine();
 
-        } while(true);
+            if(phrase.equals("1")){
+
+            }
+            else if (phrase.equals("2")){
+
+            }
+            else if (!phrase.equals("0")){
+                System.out.println("Operación no contemplada.");
+            }
+
+        } while(!phrase.equals("0"));
+
+        System.out.println("Hasta la próxima, soldado.");
     }
 }
--- src/main/java/com/PhonexConvert/PhonexConvert.java
+++ src/main/java/com/PhonexConvert/PhonexConvert.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PhonexConvert {
-    private Map<Character, String> phoneticMap = Map.ofEntries(
+    private final Map<Character, String> phoneticMap = Map.ofEntries(
             Map.entry('A', "Alpha"), Map.entry('B', "Bravo"), Map.entry('C', "Charlie"),
             Map.entry('D', "Delta"), Map.entry('E', "Echo"), Map.entry('F', "Foxtrot"),
             Map.entry('G', "Golf"), Map.entry('H', "Hotel"), Map.entry('I', "India"),
@@ -13,9 +13,22 @@ public class PhonexConvert {
             Map.entry('P', "Papa"), Map.entry('Q', "Quebec"), Map.entry('R', "Romeo"),
             Map.entry('S', "Sierra"), Map.entry('T', "Tango"), Map.entry('U', "Uniform"),
             Map.entry('V', "Victor"), Map.entry('W', "Whiskey"), Map.entry('X', "X-ray"),
-            Map.entry('Y', "Yankee"), Map.entry('Z', "Zulu")
-    );
+            Map.entry('Y', "Yankee"), Map.entry('Z', "Zulu"));
 
-    Map<String, Character> reversePhoneticMap = phoneticMap.entrySet()
+    private final Map<String, Character> reversePhoneticMap = phoneticMap.entrySet() // necessary?
             .stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+
+    public String getValue(char letter){ // Cypher
+        return phoneticMap.get(letter);
+    }
+
+    public Character getKey(String word){ // needed if reverse phoneticMap is used?
+        for (Map.Entry<Character, String> entry : phoneticMap.entrySet()) {
+            if (entry.getValue().equals(word)) {
+                return entry.getKey();
+            }
+        }
+
+        return null;
+    }
 }
